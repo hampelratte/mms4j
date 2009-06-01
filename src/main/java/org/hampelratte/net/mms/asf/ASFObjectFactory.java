c99bdd978a638ee04e0b592ad4002a8737f78fc2
hampelratte <hampelratte@e742bc6a-2767-0410-a9b8-df252b6c4596>
2009-06-01 13:01:12 +0000

    added some more asf header objects
    git-svn-id: http://svn.berlios.de/svnroot/repos/vch/trunk/mms4j@127 e742bc6a-2767-0410-a9b8-df252b6c4596

--- src/main/java/org/hampelratte/net/mms/asf/ASFObjectFactory.java
+++ src/main/java/org/hampelratte/net/mms/asf/ASFObjectFactory.java
@@ -2,21 +2,36 @@ package org.hampelratte.net.mms.asf;
 
 import java.util.HashMap;
 
+import org.hampelratte.net.mms.asf.objects.ASFCodecListObject;
 import org.hampelratte.net.mms.asf.objects.ASFContentDescriptionObject;
+import org.hampelratte.net.mms.asf.objects.ASFExtendedContentDescriptionObject;
 import org.hampelratte.net.mms.asf.objects.ASFFilePropertiesObject;
+import org.hampelratte.net.mms.asf.objects.ASFHeaderExtensionObject;
 import org.hampelratte.net.mms.asf.objects.ASFObject;
+import org.hampelratte.net.mms.asf.objects.ASFStreamBitratePropertiesObject;
+import org.hampelratte.net.mms.asf.objects.ASFStreamPropertiesObject;
 import org.hampelratte.net.mms.asf.objects.ASFToplevelHeader;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import unclealex.mms.GUID;
 
 public class ASFObjectFactory {
+    
+    private static transient Logger logger = LoggerFactory.getLogger(ASFObjectFactory.class);
+    
     private static HashMap<GUID, Class<?>> guid2Class = new HashMap<GUID, Class<?>>();
     static {
         guid2Class.put(new GUID("75B22630-668E-11CF-A6D9-00AA0062CE6C"), ASFToplevelHeader.class);
         guid2Class.put(new GUID("75B22633-668E-11CF-A6D9-00AA0062CE6C"), ASFContentDescriptionObject.class);
         guid2Class.put(new GUID("8CABDCA1-A947-11CF-8EE4-00C00C205365"), ASFFilePropertiesObject.class);
-        // TODO implement the other header objects ?!?
-        //guid2Class.put(new GUID("5FBF03B5-A92E-11CF-8EE3-00C00C205365"), ASFHeaderExtensionObject.class);
+        
+        // TODO implement these header objects ?!? at the moment they are only skeletons without functionality
+        guid2Class.put(new GUID("5FBF03B5-A92E-11CF-8EE3-00C00C205365"), ASFHeaderExtensionObject.class);
+        guid2Class.put(new GUID("D2D0A440-E307-11D2-97F0-00A0C95EA850"), ASFExtendedContentDescriptionObject.class);
+        guid2Class.put(new GUID("86D15240-311D-11D0-A3A4-00A0C90348F6"), ASFCodecListObject.class);
+        guid2Class.put(new GUID("B7DC0791-A9B7-11CF-8EE6-00C00C205365"), ASFStreamPropertiesObject.class);
+        guid2Class.put(new GUID("7BF875CE-468D-11D1-8D82-006097C9A2B2"), ASFStreamBitratePropertiesObject.class);
     }
     
     public static ASFObject createObjectFromGUID(GUID guid) throws UnknownAsfObjectException, InstantiationException, IllegalAccessException {
@@ -24,6 +39,7 @@ public class ASFObjectFactory {
         if(objectClass != null) {
             ASFObject asfo = (ASFObject) objectClass.newInstance();
             asfo.setGuid(guid);
+            logger.debug("Created ASF object of type {}", asfo.getClass().getSimpleName());
             return asfo;
         } else {
             throw new UnknownAsfObjectException(guid);
