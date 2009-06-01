e6472354b6154bfddbcc740633bd24415afc7507
hampelratte <hampelratte@e742bc6a-2767-0410-a9b8-df252b6c4596>
2009-06-01 13:02:17 +0000

    git-svn-id: http://svn.berlios.de/svnroot/repos/vch/trunk/mms4j@129 e742bc6a-2767-0410-a9b8-df252b6c4596

--- src/main/java/org/hampelratte/net/mms/test/Test.java
+++ src/main/java/org/hampelratte/net/mms/test/Test.java
@@ -1,5 +1,6 @@
 package org.hampelratte.net.mms.test;
 
+import java.io.ByteArrayInputStream;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.UUID;
@@ -10,9 +11,15 @@ import org.apache.mina.core.future.IoFutureListener;
 import org.apache.mina.core.service.IoHandlerAdapter;
 import org.apache.mina.core.session.IoSession;
 import org.hampelratte.net.mina.nio.MessageSentDumpFilter;
+import org.hampelratte.net.mms.asf.io.ASFInputStream;
+import org.hampelratte.net.mms.asf.objects.ASFObject;
+import org.hampelratte.net.mms.asf.objects.ASFToplevelHeader;
 import org.hampelratte.net.mms.client.MMSClient;
 import org.hampelratte.net.mms.client.MMSNegotiator;
 import org.hampelratte.net.mms.client.listeners.MMSMessageListener;
+import org.hampelratte.net.mms.client.listeners.MMSPacketListener;
+import org.hampelratte.net.mms.data.MMSHeaderPacket;
+import org.hampelratte.net.mms.data.MMSPacket;
 import org.hampelratte.net.mms.io.MMSFileDumper;
 import org.hampelratte.net.mms.messages.MMSMessage;
 import org.hampelratte.net.mms.messages.client.Connect;
@@ -21,38 +28,44 @@ import org.hampelratte.net.mms.messages.client.OpenFile;
 import org.hampelratte.net.mms.messages.client.ReadBlock;
 import org.hampelratte.net.mms.messages.client.StreamSwitch;
 import org.hampelratte.net.mms.messages.server.ReportStreamSwitch;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
+public class Test implements MMSMessageListener, MMSPacketListener {
 
-public class Test implements MMSMessageListener {
-    
+    private static transient Logger logger = LoggerFactory.getLogger(Test.class);
+
     private MMSClient client;
-    
+
     public Test() throws FileNotFoundException {
         // zdf lang
-        // mms://ondemand.msmedia.zdf.newmedia.nacamar.net/zdf/data/msmedia/zdf/09/02/090216_schnauzen16_nbs_vh.wmv
-//        String host = "ondemand.msmedia.zdf.newmedia.nacamar.net";
-//        String filepath = "zdf/data/msmedia/zdf/09/02/";
-//        String filename = "090216_schnauzen16_nbs_vh.wmv";
-        
+        // mms://ondemand.msmedia.zdf.newmedia.nacamar.net/zdf/data/msmedia/zdf/07/03/070313_pipeline_htc_vh.wmv
+         String host = "ondemand.msmedia.zdf.newmedia.nacamar.net";
+         String filepath = "zdf/data/msmedia/zdf/07/03/";
+         String filename = "070313_pipeline_htc_vh.wmv";
+
         // zdf kurz
-//      String host = "c36000-o.w.core.cdn.streamfarm.net";
-//      String filepath = "36000zdf/ondemand/3546zdf/zdf/zdf/08/03/";
-//      String filename = "080316_wiibowling_nes_vh.wmv";
+        // mms://ondemand.msmedia.zdf.newmedia.nacamar.net/zdf/data/msmedia/3sat/09/05/090524_sendung2_neues_vh.wmv
+//         String host = "ondemand.msmedia.zdf.newmedia.nacamar.net";
+//         String filepath = "zdf/data/msmedia/3sat/09/05/";
+//         String filename = "090524_sendung2_neues_vh.wmv";
 
         // arte
-        String host = "a764.v39759a.c39759.g.vm.akamaistream.net";
-        String filepath = "7/764/39759/0160948f76f2a16a1692ad36588392de/artegeie.download.akamai.com/39759/mfile/arteprod/";
-        String filename = "A7_SGT_ENC_04_032463-000-A_PG_HQ_DE.wmv";
-        
+        // mms://a1091.v397591.c39759.g.vm.akamaistream.net/7/1091/39759/0b48fbf5073e3b715dad199ef8df723d/artegeie.download.akamai.com/39759/mfile/arteprod/A7_SGT_ENC_04_039360-000-A_PG_HQ_DE.wmv
+//        String host = "a311.v397595.c39759.g.vm.akamaistream.net";
+//        String filepath = "7/1091/39759/0b48fbf5073e3b715dad199ef8df723d/artegeie.download.akamai.com/39759/mfile/arteprod/";
+//        String filename = "A7_SGT_ENC_04_039360-000-A_PG_HQ_DE.wmv";
+
         // rtl
-//        String host = "217.118.170.35";
-//        String filepath = "vod/57/";
-//        String filename = "RTL_achtunghartwich_080309_700k.wmv?ssid=a18561c909ba12eae66388c47059170f&cid=1205786160&rd=1&rnd=7387286";
+        // String host = "217.118.170.35";
+        // String filepath = "vod/57/";
+        // String filename =
+        // "RTL_achtunghartwich_080309_700k.wmv?ssid=a18561c909ba12eae66388c47059170f&cid=1205786160&rd=1&rnd=7387286";
 
         MMSNegotiator negotiator = new MMSNegotiator();
         client = new MMSClient(host, 1755, negotiator);
         negotiator.setClient(client);
-        
+
         // configure the negotiator
         // connect
         Connect connect = new Connect();
@@ -78,13 +91,14 @@ public class Test implements MMSMessageListener {
         ss.addStreamSwitchEntry(ss.new StreamSwitchEntry(0xFFFF, 1, 0));
         ss.addStreamSwitchEntry(ss.new StreamSwitchEntry(0xFFFF, 2, 0));
         negotiator.setStreamSwitch(ss);
-                
+
         // dump the received header and media packets to a file
         File dump = new File("/tmp", filename);
         MMSFileDumper dumper = new MMSFileDumper(dump);
         client.addMessageListener(dumper);
         client.addMessageListener(this);
         client.addPacketListener(dumper);
+        client.addPacketListener(this);
 
         client.addAdditionalIoHandler(new IoHandlerAdapter() {
             @Override
@@ -105,12 +119,18 @@ public class Test implements MMSMessageListener {
                 });
             }
         });
+
         // finally start the download, the negotiator
         // will start automatically when the connection is
         // established
-        client.connect();
+        try {
+            client.connect();
+        } catch (Exception e) {
+            System.err.println("Couldn't connect to host");
+            e.printStackTrace(System.err);
+        }
     }
-    
+
     /**
      * @param args
      * @throws FileNotFoundException
@@ -120,12 +140,36 @@ public class Test implements MMSMessageListener {
     }
 
     public void messageReceived(MMSMessage command) {
-        if(command instanceof ReportStreamSwitch) {
+        if (command instanceof ReportStreamSwitch) {
             // great, we can start the streaming
             long startPacket = 0;
-            
+
             // start the streaming
             client.startStreaming(startPacket);
-        } 
+        }
+    }
+
+    @Override
+    public void packetReceived(MMSPacket mmspacket) {
+        if (mmspacket instanceof MMSHeaderPacket) {
+            MMSHeaderPacket hp = (MMSHeaderPacket) mmspacket;
+            ByteArrayInputStream bin = new ByteArrayInputStream(hp.getData());
+            ASFInputStream asfin = new ASFInputStream(bin);
+
+            ASFObject asfo = null;
+            int count = 0;
+            while( !(asfo instanceof ASFToplevelHeader) && count++ < 5) {
+                try {
+                    asfo = asfin.readASFObject();
+                } catch (Exception e) {
+                    logger.warn("Ignoring unknown ASF header object", e);
+                }   
+            }
+
+            if(asfo != null) {
+                ASFToplevelHeader asfHeader = (ASFToplevelHeader) asfo;
+                logger.debug("ASF header: {}", asfHeader);
+            }
+        }
     }
 }
