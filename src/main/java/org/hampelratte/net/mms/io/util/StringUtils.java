1cbfadc5f8b3b704b33a5582e58874bc14b080f1
Henrik Niehaus <henrik.niehaus@gmx.de>
2011-09-17 22:51:38 +0200

    added 2 new methods to print out IoBuffers directly

--- src/main/java/org/hampelratte/net/mms/io/util/StringUtils.java
+++ src/main/java/org/hampelratte/net/mms/io/util/StringUtils.java
@@ -4,61 +4,80 @@ import java.nio.charset.Charset;
 import java.nio.charset.CharsetDecoder;
 import java.nio.charset.CharsetEncoder;
 
+import org.apache.mina.core.buffer.IoBuffer;
+
 public class StringUtils {
+
+    public static String toHexString(IoBuffer b) {
+        return toHexString(b, 8);
+    }
+
+    public static String toHexString(IoBuffer b, int bytesPerRow) {
+        b.mark();
+        byte[] bytes = new byte[b.remaining()];
+        b.get(bytes);
+        b.reset();
+        return toHexString(bytes, bytesPerRow);
+    }
+
     public static String toHexString(byte[] bytes, int bytesPerRow) {
-        if(bytes == null) {
+        if (bytes == null) {
             return "";
         }
-        
+
         StringBuilder sb = new StringBuilder();
-        for (int i = 0; i < bytes.length; i+=bytesPerRow) {
+        for (int i = 0; i < bytes.length; i += bytesPerRow) {
             int length = bytes.length - i >= bytesPerRow ? bytesPerRow : bytes.length % bytesPerRow;
             byte[] row = new byte[bytesPerRow];
             System.arraycopy(bytes, i, row, 0, length);
-            
+
             for (int j = 0; j < length; j++) {
-                sb.append('0'); sb.append('x');
+                sb.append('0');
+                sb.append('x');
                 sb.append(toHexString(row[j]));
                 sb.append(' ');
             }
-            
+
             for (int j = 0; j < bytesPerRow - length; j++) {
                 sb.append("     ");
             }
-            
+
             for (int j = 0; j < Math.min(length, bytesPerRow); j++) {
-                if(row[j] >= 32 && row[j] <= 127) {
-                    sb.append((char)row[j]);
+                if (row[j] >= 32 && row[j] <= 127) {
+                    sb.append((char) row[j]);
                 } else {
                     sb.append('.');
                 }
             }
-            
-            if(i+bytesPerRow < bytes.length) {
-                sb.append('\n'); sb.append(' '); sb.append(' ');
+
+            if (i + bytesPerRow < bytes.length) {
+                sb.append('\n');
+                sb.append(' ');
+                sb.append(' ');
             }
         }
         return sb.toString();
     }
-    
+
     /**
-     * Converts one byte to its hex representation with leading zeros.
-     * E.g. 255 -> FF
+     * Converts one byte to its hex representation with leading zeros. E.g. 255 -> FF
+     * 
      * @param b
      * @return the hex representation of the <code>b</code>
      */
     public static String toHexString(int b) {
         String hex = Integer.toHexString(b & 0xFF);
-        if(hex.length() < 2) {
+        if (hex.length() < 2) {
             hex = "0" + hex;
         }
         return hex;
     }
-    
+
     public static final int LOG_BYTE_DUMP_SIZE = 8 * 4;
-    
+
     /**
      * Creates a hexdump with a maximum length of {@link StringUtils#LOG_BYTE_DUMP_SIZE} bytes
+     * 
      * @param data
      * @return hexdump as String
      */
@@ -66,29 +85,31 @@ public class StringUtils {
         int length = Math.min(data.length, LOG_BYTE_DUMP_SIZE);
         byte[] head = new byte[length];
         System.arraycopy(data, 0, head, 0, length);
-        
+
         StringBuilder sb = new StringBuilder(toHexString(head, 8));
         sb.append("\n  ...");
         return sb.toString();
-    }   
-    
+    }
+
     public static String toCharString(byte[] bytes) {
         StringBuilder sb = new StringBuilder();
         for (int i = 0; i < bytes.length; i++) {
-            char c = (char)(bytes[i] & 0xFF);
+            char c = (char) (bytes[i] & 0xFF);
             sb.append(c);
             sb.append(' ');
-            if(i % 8 == 7) {
-                sb.append('\n'); sb.append(' '); sb.append(' ');
+            if (i % 8 == 7) {
+                sb.append('\n');
+                sb.append(' ');
+                sb.append(' ');
             }
         }
         return sb.toString();
     }
-    
+
     public static CharsetEncoder getEncoder(String charset) {
         return Charset.forName(charset).newEncoder();
     }
-    
+
     public static CharsetDecoder getDecoder(String charset) {
         return Charset.forName(charset).newDecoder();
     }
